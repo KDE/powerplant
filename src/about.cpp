dc8146ba8848f6cc9acebc854e1378d1c430c0ea
Carl Schwan <carl@carlschwan.eu>
2023-04-12 07:02:46 +0000

    Simplify about page

--- src/about.cpp
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-// PDX-FileCopyrightText: 2023 Mathis <mbb@kaidan.im>
-
-#include "about.h"
-
-KAboutData AboutType::aboutData() const
-{
-    return KAboutData::applicationData();
-}
